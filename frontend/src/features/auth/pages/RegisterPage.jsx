50eee95e8545e43047ffe40d85bb23528362de6a
Yeider <yeidercatari.dev@gmail.com>
2026-04-29 20:54:57 -0400

    feat(auth): agregar páginas y componentes de registro

---
+++ frontend/src/features/auth/pages/RegisterPage.jsx
@@ -0,0 +1,12 @@
+import AuthLayout from '../components/AuthLayout';
+import RegisterForm from '../components/RegisterForm';
+
+function RegisterPage() {
+  return (
+    <AuthLayout>
+      <RegisterForm />
+    </AuthLayout>
+  );
+}
+
+export default RegisterPage;
