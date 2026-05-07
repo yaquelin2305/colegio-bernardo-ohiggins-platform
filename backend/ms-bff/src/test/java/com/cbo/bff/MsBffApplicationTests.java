06a51e9287b02407b82fe2796dc1e7383716bf02
Yeider <yeidercatari.dev@gmail.com>
2026-05-06 21:28:41 -0400

    feat(bff): configuración inicial del microservicio BFF

---
+++ backend/ms-bff/src/test/java/com/cbo/bff/MsBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbo.bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
